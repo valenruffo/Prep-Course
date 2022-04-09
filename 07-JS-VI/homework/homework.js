8a7a549ac0b1f0f93d4c65edc6bd235d3d1992a1
Valentino <valentinoruffo2016@gmail.com>
2022-04-09 11:32:17 -0300

    JS-VI Finished

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,22 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  mayusc = nombre[0].toUpperCase() + nombre.slice(1);
+  return mayusc;
+
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1 , n1);
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +27,19 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  let sumarNum = numeros.reduce(function(acc,ele){
+    return acc + ele;
+  });
+  cb(sumarNum);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach (function(ele) {
+    cb(ele);
+  });
 }
 
 function map(array, cb) {
@@ -35,12 +47,21 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  let newArray = array.map(function (ele) {
+    return cb(ele);
+  });
+  return newArray;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  let newArray = array.filter(function (ele) {
+    return ele[0] === 'a';
+  })
+  return newArray;
+
 }
 
 // No modificar nada debajo de esta línea
